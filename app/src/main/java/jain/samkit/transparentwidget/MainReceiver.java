85677d440ba552d7eb6cb189216373dd21c41cd8
Samkit Jain <samkit@btech-d-13-samkit-7797.(none)>
2016-01-31 11:45:10 +0530

    Initial commit

---
+++ app/src/main/java/jain/samkit/transparentwidget/MainReceiver.java
@@ -0,0 +1,11 @@
+package jain.samkit.transparentwidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class MainReceiver extends AppWidgetProvider {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        //do nothing
+    }
+}
